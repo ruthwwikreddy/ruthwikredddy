318859b730c0734e72f6cab53746bd9b0d7f3486
Akkenapally Ruthwik Reddy <131450727+ruthwwikreddy@users.noreply.github.com>
2025-05-16 18:58:22 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,14 +15,13 @@ export default {
 			center: true,
 			padding: {
 				DEFAULT: '1rem',
-				sm: '1.5rem',
-				md: '2rem',
+				sm: '2rem',
 				lg: '4rem',
 				xl: '5rem',
 				'2xl': '6rem',
 			},
 			screens: {
-				sm: '360px',
+				sm: '640px',
 				md: '768px',
 				lg: '1024px',
 				xl: '1280px',
