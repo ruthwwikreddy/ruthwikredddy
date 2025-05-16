ad3131a69a4a7afa493bacb9d073939daf2330e3
Your Name <you@example.com>
2025-05-16 14:29:02 +0530

    Initial commit for Instagram Bio project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ruthwikredddy/', // this should match your repo nam
   server: {
     host: "::",
     port: 8080,
